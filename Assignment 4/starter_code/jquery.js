6ab33d805a54d9966ce6704b83d0afeda1c3da00
Nayana Davis <nayana487@gmail.com>
2016-01-29 16:23:18 -0500

    added comments

--- Assignment 4/starter_code/jquery.js
+++ Assignment 4/starter_code/jquery.js
@@ -5,26 +5,26 @@ $("#").click(function(event) {
 
  $( document ).ready(function() {
   });
-
+// ND: Remember, the document ready function should be at the top and encompass all
+// your jquery
 
 $(".readmore").click(function(){
 	$("#show-this-on-click").slideDown();
 	$(".readmore").hide();
 	$(".readless.hide").show();
 });
+// ND: So this should be slideDown, shown, then hide
 
 $(".readless.hide").click(function(){
   $("#show-this-on-click").slideUp();
   $(".readmore").show();
   $(".readless.hide").hide();
-  
+  // ND:Here, SlideUp, hide then show
+
 });
 
 $(".learnmore").click(function(){
 	$("#learnmoretext").slideDown();
 	$(".learnmore").hide();
+  // ND: Nice, you got this!
 });
-
-
-
-
